65aaa61502686459a540b04b7d67808b34aeea74
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 20:12:22 -0300

    feat: adicionado permissão qualquer usuário cadastrar, e tmb um tratamento temporário à emails e números de telefone duplicados no banco

--- Controller/CreateUserAccountController.cs
+++ Controller/CreateUserAccountController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Projexor.Controller.DTO;
 using Projexor.Data.Context;
 using Projexor.Models;
@@ -12,6 +14,7 @@ namespace Projexor.Controller;
 public class CreateUserAccount : ControllerBase
 {
     [HttpPost("user")]
+    [AllowAnonymous]
     public async Task<IActionResult> CreateAsync([FromBody] CreateUserAccountDTO user_dto, [FromServices] AppDbContext context)
     {
         try
@@ -27,8 +30,26 @@ public class CreateUserAccount : ControllerBase
             return StatusCode(200, new { Message = "Conta criada com sucesso.", TokenKey = JwtServices.GenerateToken(userAccount), Error = "[]" });
         }
 
+        catch (DbUpdateException e)
+        {
+            string? msg = null;
+
+            if (e != null)
+            {
+                if (e.InnerException.Message.Contains("UserAccount.Email"))
+                    msg = "Email já existe";
+
+                else if (e.InnerException.Message.Contains("UserAccount.PhoneNumber"))
+                    msg = "PhoneNumber já existe";
+            }
+
+            return BadRequest(new { Message = "Erro ao adicionar conta do usuário", Error = msg ?? e.InnerException?.Message ?? e.Message ?? "Erro desconhecido." });
+        }
+
         catch (Exception e)
         {
+
+
             return StatusCode(500, new { Message = "Erro interno do servidor.", Error = e.InnerException?.Message ?? e.Message ?? "Erro desconhecido." });
         }
     }
