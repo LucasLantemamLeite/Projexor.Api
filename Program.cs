28893ce7277839d2afce87afc9716ed12d290058
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 19:12:32 -0300

    feat: adicionado injeção de dependência para o banco e suporte a controllers

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using Projexor.Data.Context;
 using Projexor.Services.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
 JwtSettings.JwtToken = builder.Configuration.GetValue<string>("JwtSettings:JwtToken") ?? string.Empty;
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
+app.MapControllers();
 
 app.Run();
