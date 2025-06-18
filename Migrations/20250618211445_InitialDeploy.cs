71dd2838244dd3f0a119a483a039b38e0193e69d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 18:17:58 -0300

    chore: arquivos de migração criado automaticamente pelo EF

---
+++ Migrations/20250618211445_InitialDeploy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projexor.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Role = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccount", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Email_UserAccount",
+                table: "UserAccount",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_PhoneNumber_UserAccount",
+                table: "UserAccount",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccount");
+        }
+    }
+}
