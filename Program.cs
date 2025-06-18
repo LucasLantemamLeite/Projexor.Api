ec7befc9708d43673b46fda69632536be43ba922
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 18:36:04 -0300

    feat: atribuido a TokenKey do appsettings a JwtToken

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Projexor.Services.Token;
+
 var builder = WebApplication.CreateBuilder(args);
+
+JwtSettings.JwtToken = builder.Configuration.GetValue<string>("JwtSettings:JwtToken") ?? string.Empty;
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
