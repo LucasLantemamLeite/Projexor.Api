f3b7b368afdaf44a796f7d81151d70cac8901042
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 17:54:20 -0300

    feat: criação do model UserAccount para representação da conta do usuário

---
+++ Models/UserAccountModel.cs
@@ -0,0 +1,25 @@
+namespace Projexor.Models;
+
+public class UserAccount
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public DateTime CreateAt { get; set; }
+    public bool Active { get; set; }
+    public int Role { get; set; }
+
+    public UserAccount(string name, string email, string phone_number, DateOnly birth_date, bool active = true, int role = 1)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        PhoneNumber = phone_number;
+        BirthDate = birth_date;
+        CreateAt = DateTime.UtcNow;
+        Active = active;
+        Role = role;
+    }
+}
