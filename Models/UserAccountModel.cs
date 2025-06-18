1d609125025e1592a254dd60c0936e80e13cf616
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 18:12:41 -0300

    fix: adicionado a propriedade PasswordHash que faltou no Model

--- Models/UserAccountModel.cs
+++ Models/UserAccountModel.cs
@@ -6,15 +6,17 @@ public class UserAccount
     public string Name { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
+    public string PasswordHash { get; set; }
     public DateOnly BirthDate { get; set; }
     public DateTime CreateAt { get; set; }
     public bool Active { get; set; }
     public int Role { get; set; }
 
-    public UserAccount(string name, string email, string phone_number, DateOnly birth_date, bool active = true, int role = 1)
+    public UserAccount(string name, string email, string password, string phone_number, DateOnly birth_date, bool active = true, int role = 1)
     {
         Id = Guid.NewGuid();
         Name = name;
+        PasswordHash = password;
         Email = email;
         PhoneNumber = phone_number;
         BirthDate = birth_date;
