8bc274f4b57d30e28103ad4a3ff01c1f8dc2ea0d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-18 19:13:57 -0300

    feat: criação da rota de cadastro do usuário

---
+++ Controller/CreateUserAccountController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Projexor.Controller.DTO;
+using Projexor.Data.Context;
+using Projexor.Models;
+using Projexor.Services.Token;
+
+namespace Projexor.Controller;
+
+[ApiController]
+[Route("v1")]
+public class CreateUserAccount : ControllerBase
+{
+    [HttpPost("user")]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateUserAccountDTO user_dto, [FromServices] AppDbContext context)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userAccount = new UserAccount(user_dto.Name, user_dto.Email, user_dto.Password, user_dto.PhoneNumber, user_dto.BirthDate);
+
+            await context.UserAccounts.AddAsync(userAccount);
+            // await context.SaveChangesAsync();
+
+            return StatusCode(200, new { Message = "Conta criada com sucesso.", TokenKey = JwtServices.GenerateToken(userAccount), Error = "[]" });
+        }
+
+        catch (Exception e)
+        {
+            return StatusCode(500, new { Message = "Erro interno do servidor.", Error = e.InnerException?.Message ?? e.Message ?? "Erro desconhecido." });
+        }
+    }
+}
