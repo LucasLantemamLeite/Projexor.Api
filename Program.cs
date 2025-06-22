6d9ef7b8e50e015d301b3383684d69a20eedefab
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-21 21:49:57 -0300

    feat: adição da configuração de authentication da Token Key e configuração do Swagger em Desenvolvimento

--- Program.cs
+++ Program.cs
@@ -1,13 +1,45 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Projexor.Data.Context;
 using Projexor.Services.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
 JwtSettings.JwtToken = builder.Configuration.GetValue<string>("JwtSettings:JwtToken") ?? string.Empty;
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => x.TokenValidationParameters = new TokenValidationParameters
+{
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtSettings.JwtToken)),
+    ValidateIssuer = false,
+    ValidateAudience = false
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
